74fca36d52a4ddda101e7a5cb367e7b337ebf087
Kanwarjit Rupyal <kanwar.rupyal@gmail.com>
2018-12-20 13:39:16 -0600

    init-Player class made, working on create gesture method

---
+++ RPSLS/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    class Player
+    {
+        //Member Variables
+        public string name;
+
+        //Constructor
+        public Player(string name)
+        {
+            this.name = name;
+        }
+
+        //Member Methods
+        public string createGesture()
+        {
+            List<string> gestures = new List<string>();
+            gestures.Add("Rock");
+            gestures.Add("Paper");
+            gestures.Add("Scissors");
+            gestures.Add("Lizard");
+            gestures.Add("Spock");
+        }
+
+    }
+}
